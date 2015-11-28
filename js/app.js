05ba24522d8a0865430fda77620f36585101f200
Riccardo Salucco <riccardo@notthatred.com>
2015-11-28 12:06:39 +0100

    Creating a function to initializeMap

--- js/app.js
+++ js/app.js
@@ -12,3 +12,30 @@ var BetweenViewModel = function() {
     project.showList = ko.observable(false); 
     project.selectedPlaceId = ko.observable(false);
 
+    /* Create an initializeMap function*/
+    project.initializeMap = function() {
+        project.latlong = new google.maps.LatLng(46.786678, -92.100495);
+
+        var mapOptions = {
+            center: project.latlong,
+            zoom: 18,
+            panControl: true,
+            panControlOptions: {
+                position: google.maps.ControlPosition.RIGHT_BOTTOM
+            },
+            zoomControl: true,
+            zoomControlOptions: {
+                position: google.maps.ControlPosition.RIGHT_BOTTOM
+            },
+            scaleControl: true,
+            streetViewControl: true,
+            streetViewControlOptions: {
+                position: google.maps.ControlPosition.RIGHT_BOTTOM
+            }
+        };
+        project.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        project.searchLocations();
+        project.map.setCenter(project.latlong);
+    };
+
+    
